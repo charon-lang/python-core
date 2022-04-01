057203d6e878daae1f9968fd05361492f0922142
James Butcher <jamesbutcher167@gmail.com>
2022-04-01 22:35:47 +0100

    Replace eval with State::apply

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use pyo3::prelude::*;
 use pyo3::types::*;
-use wcore::models::WTokens;
 use std::ffi;
 use std::ptr::addr_of;
-use wcore;
-use wcore::models::{Range, Token::*};
+
+use wcore::{
+    evaluator::WEval,
+    models::{Range, State, Token::*, WTokens},
+};
 
 fn create_pylist<'a>(py: Python, acc: &'a PyList, wcode: Vec<WTokens>) -> &'a PyList {
-     for section in wcode {
+    for section in wcode {
         for code_result in section {
             match code_result {
                 Container(x) | ContainerLiteral(x) | Atom(x) | Special(x) => acc.append(x).unwrap(),
@@ -17,12 +19,16 @@ fn create_pylist<'a>(py: Python, acc: &'a PyList, wcode: Vec<WTokens>) -> &'a Py
                     let slice = unsafe { ffi::CStr::from_ptr(address) };
                     acc.append(slice.to_string_lossy().into_owned()).unwrap()
                 }
-                Parameter(x) => acc.append(match x {
-                    Range::Full(y) => format!("{}..={}", y.start(), y.end()),
-                    Range::To(y) => format!("..{}", y.start),
-                    Range::From(y) => format!("{}..", y.end),
-                }).unwrap(),
-                Group(x) => acc.append(create_pylist(py, PyList::empty(py), vec![x])).unwrap(),
+                Parameter(x) => acc
+                    .append(match x {
+                        Range::Full(y) => format!("{}..={}", y.start(), y.end()),
+                        Range::To(y) => format!("..{}", y.start),
+                        Range::From(y) => format!("{}..", y.end),
+                    })
+                    .unwrap(),
+                Group(x) => acc
+                    .append(create_pylist(py, PyList::empty(py), vec![x]))
+                    .unwrap(),
             };
         }
     }
@@ -30,18 +36,31 @@ fn create_pylist<'a>(py: Python, acc: &'a PyList, wcode: Vec<WTokens>) -> &'a Py
     acc
 }
 
-#[pyfunction]
-fn eval(py: Python, code: String) -> PyResult<&PyAny> {
-    let result = PyList::empty(py);
-    let code_eval = wcore::eval(&code);
+#[pyclass(name = "State")]
+struct PyState {
+    state: State,
+}
 
-    create_pylist(py, result, code_eval);
+#[pymethods]
+impl PyState {
+    #[new]
+    fn __new__() -> PyResult<Self> {
+        Ok(Self {
+            state: State::new(),
+        })
+    }
 
-    Ok(result.downcast()?)
+    fn apply<'a>(&mut self, py: Python<'a>, code: String) -> PyResult<&'a PyAny> {
+        let result = PyList::empty(py);
+        let code_eval = self.state.apply(&code);
+
+        create_pylist(py, result, code_eval);
+        Ok(result.downcast()?)
+    }
 }
 
 #[pymodule]
 fn wcore_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(eval, m)?)?;
+    m.add_class::<PyState>()?;
     Ok(())
 }
